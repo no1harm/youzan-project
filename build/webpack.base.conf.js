9cce9690141c9a364278b818d5ef6a1a76583362
no1harm <chen09869@gmail.com>
2018-10-30 22:48:25 +0800

    多页面应用调整：
    1.项目目录结构调整
    
    2.webpack配置调整

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -12,9 +12,10 @@ function resolve (dir) {
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
-  entry: {
-    app: './src/main.js'
-  },
+  // entry: {
+  //   app: './src/main.js'
+  // },
+  entry: utils.entries(),
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
