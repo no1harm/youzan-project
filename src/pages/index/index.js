27c31554a77635b096bf1e21855fbacd32d10699
no1harm <chen09869@gmail.com>
2018-10-31 23:34:24 +0800

    使用 mint-ui 做 Infinite scroll，实现数据加载

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,19 +3,43 @@ import './index.css'
 import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
+import { InfiniteScroll } from 'mint-ui'
+Vue.use(InfiniteScroll)
 
 let app = new Vue({
     el:'#app',
     data:{
-        lists:null
+        lists:null,
+        pageNum:1,
+        pageSize:6,
+        loading:false,
+        allLoaded:false
     },
     created() {
-        axios.post(url.hotLists,{
-            pageNum:1,
-            pageSize:6
-        }).then(res=>{
-            this.lists = res.data.lists
-            console.log(this.lists)
-        })
+        this.getLists()
     },
+    methods:{
+        getLists(){
+            if(this.allLoaded) return
+            this.loading = true
+            axios.post(url.hotLists,{
+                pageNum:this.pageNum,
+                pageSize:this.pageSize
+            }).then(res=>{
+                let curLists = res.data.lists
+                // 判断所有数据是否加载完毕
+                if(curLists.length < this.pageSize){
+                    this.allLoaded = true
+                }
+                if(this.lists){
+                    this.lists = this.lists.concat(curLists)
+                }else{
+                    // 第一次请求数据
+                    this.lists = res.data.lists
+                }
+            })
+            this.loading = false
+            this.pageNum ++
+        }
+    }
 })
