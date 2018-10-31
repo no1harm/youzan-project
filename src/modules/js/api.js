c4fe639e75ad6d48080609711f08141029b5bbe3
no1harm <chen09869@gmail.com>
2018-10-31 20:18:18 +0800

    封装API，获取数据，渲染模板

---
+++ src/modules/js/api.js
@@ -0,0 +1,14 @@
+let url = {
+    hotLists:'/index/hotLists'
+}
+
+// 开发环境和真实环境的切换
+let host =  'http://rap2api.taobao.org/app/mock/7058'
+
+for (let key in url){
+    if(url.hasOwnProperty(key)){
+        url[key] = host + url[key]
+    }
+}
+
+export default url
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,2 +1,21 @@
 import 'css/common.css'
 import './index.css'
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+
+let app = new Vue({
+    el:'#app',
+    data:{
+        lists:null
+    },
+    created() {
+        axios.post(url.hotLists,{
+            pageNum:1,
+            pageSize:6
+        }).then(res=>{
+            this.lists = res.data.lists
+            console.log(this.lists)
+        })
+    },
+})
