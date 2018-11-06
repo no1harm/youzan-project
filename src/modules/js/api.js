125e2a5646490780f721798b0863fe9feeb21ba8
no1harm <chen09869@gmail.com>
2018-11-06 13:14:58 +0800

    初步渲染购物车页面

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -7,7 +7,11 @@ let url = {
     searchList:'/search/list',
     details:'/goods/details',
     deal:'/goods/deal',
-    addCart:'/cart/add'
+    addCart:'/cart/add',
+    cartList:'/cart/list',
+    cartReduce:'/cart/reduce',
+    cartRemove:'/cart/remove',
+    cartMremove:'/cart/mremove'
 }
 
 // 开发环境和真实环境的切换
--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,3 +1,63 @@
 import './cart_base.css'
 import './cart_trade.css'
 import './cart.css'
+
+import url from 'js/api.js'
+import Vue from 'vue'
+import axios from 'axios'
+import qs from 'qs'
+import mixin from 'js/mixin'
+
+new Vue({
+    el:'.container',
+    data:{
+        lists:null
+    },
+    computed:{},
+    created(){
+        this.getList()
+    },
+    methods:{
+        getList(){
+            axios.get(url.cartList).then(res=>{
+                this.lists = res.data.cartList
+            })
+        }
+    },
+    mixins:[mixin]
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Mock from 'mockjs'
+
+// let Random = Mock.Random
+
+// let data = Mock.mock({
+//     'cartList|3':[{
+//         'goodsList|1-5':[{
+//             id:Random.int(10000,100000),
+//             img:Mock.mock('@Img(90x90,@color)')
+//         }]
+//     }]
+// })
+
+// console.log(data)
+
