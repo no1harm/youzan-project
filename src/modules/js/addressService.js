ccc36828d9be76fb4c2413910055d88db25b65f6
no1harm <chen09869@gmail.com>
2018-11-07 20:47:22 +0800

    封装address相关接口

---
+++ src/modules/js/addressService.js
@@ -0,0 +1,22 @@
+import url from 'js/api.js'
+import fetch from './fetch.js'
+
+class Address {
+    static list(){
+        return fetch(url.addressLists)
+    }
+    static add(data){
+        return fetch('post',addressAdd,data)
+    }
+    static remove(id){
+        return fetch('post',url.addressRemove,id)
+    }
+    static upddate(data){
+        return fetch('post',url.addressUpdate,data)
+    }
+    static setDefault(id){
+        return fetch('post',url.addressSetDefault,id)
+    }
+}
+
+export default Address
