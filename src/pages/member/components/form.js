a686b41969d21540991db47d09c16777b51154c2
no1harm <chen09869@gmail.com>
2018-11-08 13:29:59 +0800

    处理选择地址的切换

--- src/pages/member/components/form.js
+++ src/pages/member/components/form.js
@@ -9,7 +9,10 @@ export default {
             address:'',
             id:'',
             type:'',
-            instance:''
+            instance:'',
+            addressData:require('js/address.json'),
+            cityList:null,
+            districList:null
         }
     },
     created() {
@@ -17,4 +20,30 @@ export default {
         this.type = query.type  
         this.instance = query.instance
     },
+    methods:{
+        add(){
+            
+        }
+    },
+    watch:{
+        provinceValue(val){
+            if(val===-1) return
+            let list = this.addressData.list
+            let index = list.findIndex(item=>{
+                return item.value === val
+            })
+            this.cityList = list[index].children
+            this.cityValue = -1
+            this.districtValue = -1
+        },
+        cityValue(val){
+            if(val===-1) return
+            let list = this.cityList
+            let index = list.findIndex(item=>{
+                return item.value === val
+            })
+            this.districList = list[index].children
+            this.districtValue = -1
+        }
+    }
 }
